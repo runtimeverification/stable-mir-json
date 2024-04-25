acf8d2f2a7f4fb7a46bd37416dd2911513ddc4fe
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2024-04-25 19:25:01 -0400

    print generic parameter chain

--- src/printer.rs
+++ src/printer.rs
@@ -10,6 +10,18 @@ use rustc_span::def_id::DefId;
 use rustc_smir::rustc_internal;
 use stable_mir::{CrateDef,Symbol};
 
+pub fn print_generics_chain(tcx: TyCtxt<'_>, opt_id: Option<DefId>) -> String {
+  if let Some(id) = opt_id {
+     let params = tcx.generics_of(id);
+     let preds  = tcx.predicates_of(id);
+     if params.parent != preds.parent { panic!("Generics and predicates parent ids are distinct"); }
+     let parent_chain = print_generics_chain(tcx, params.parent);
+     return format!("\nParams: {:#?}\nPreds: {:#?}\n{parent_chain}", params, preds);
+  } else {
+    return "".into()
+  }
+}
+
 pub fn print_item(tcx: TyCtxt<'_>, item: &stable_mir::CrateItem, out: &mut io::Stdout) {
   let _ = item.emit_mir(out);
   println!("{:?}", item.body());
@@ -28,14 +40,13 @@ pub fn print_item_details(tcx: TyCtxt<'_>, item: &stable_mir::CrateItem) {
   // get DefPath for item
   let path: DefPath = tcx.def_path(id);
   // get string version of DefPath
-  let path_str: String = tcx.def_path_str(id); 
+  let path_str: String = tcx.def_path_str(id);
   // get type, generic parameters, required predicates, layout
   let ty = tcx.type_of(id);
-  let params = tcx.generics_of(id);
-  let predicates = tcx.predicates_of(id);
+  let generics_chain = print_generics_chain(tcx, Some(id));
   // let layout = tcx.layout_of(id);
-  println!("===Internal===\nDefId: {:#?}\nDefPath: {:#?}\nDefPathStr: {}\nTy: {:#?}\nParams: {:#?}\nPreds: {:#?}",
-           id, path, path_str, ty, params, predicates);
+  println!("===Internal===\nDefId: {:#?}\nDefPath: {:#?}\nDefPathStr: {}\nTy: {:#?}{generics_chain}",
+           id, path, path_str, ty);
 
   // Stable Details
   //
