96545f0288eed92bb556d385a1dcf220216eebc2
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2024-06-07 16:17:27 -0500

    improve arch detection

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 set -eu
 BIN=smir_pretty
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ARCH=$("$SCRIPT_DIR/rustc_arch.sh")
 SEP=""
 [ -n "${LD_LIBRARY_PATH:-}" ] && SEP=":"
-export LD_LIBRARY_PATH="$SCRIPT_DIR/deps/rust/src/build/x86_64-unknown-linux-gnu/stage2/lib$SEP${LD_LIBRARY_PATH:-}"
+export LD_LIBRARY_PATH="$SCRIPT_DIR/deps/rust/src/build/${ARCH}/stage2/lib$SEP${LD_LIBRARY_PATH:-}"
 if [ -x "$SCRIPT_DIR/target/debug/$BIN" ]; then
   "$SCRIPT_DIR/target/debug/$BIN"   "$@"
 elif [ -x "$SCRIPT_DIR/target/release/$BIN" ]; then
