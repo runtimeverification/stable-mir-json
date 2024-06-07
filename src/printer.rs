c292d0d4fb253d12f0f061fa67d4b2ed83eea869
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2024-06-06 23:39:58 -0500

    make printer emit JSON consistently

--- src/printer.rs
+++ src/printer.rs
@@ -1,50 +1,72 @@
 use std::io;
+use std::iter::Iterator;
+use std::vec::Vec;
+use std::str;
 extern crate rustc_hir;
 extern crate rustc_middle;
 extern crate rustc_span;
 extern crate rustc_smir;
 extern crate stable_mir;
 use rustc_hir::{def::DefKind, definitions::DefPath};
-use rustc_middle::ty::{TyCtxt, Ty, TyKind, EarlyBinder, Binder, FnSig, GenericArgs, TypeFoldable};
+use rustc_middle::ty::{TyCtxt, Ty, TyKind, EarlyBinder, Binder, FnSig, GenericArgs, TypeFoldable, Generics, GenericPredicates};
 use rustc_span::{def_id::DefId, symbol::sym};
 use rustc_smir::rustc_internal;
-use stable_mir::{CrateDef,Symbol,to_json};
+use stable_mir::{CrateDef,ItemKind,Symbol,to_json,mir::Body};
 use super::pretty::function_body;
+#[macro_use]
+use tracing::enabled;
+use serde::Serialize;
 
-pub fn print_generics_chain(tcx: TyCtxt<'_>, opt_id: Option<DefId>) -> String {
-  if let Some(id) = opt_id {
-     let params = tcx.generics_of(id);
-     let preds  = tcx.predicates_of(id);
-     if params.parent != preds.parent { panic!("Generics and predicates parent ids are distinct"); }
-     let parent_chain = print_generics_chain(tcx, params.parent);
-     // skip printing empty predicate structs
-     let preds_string = if preds.predicates.len() == 0 {
-       "".into()
-     } else {
-       format!("\nPreds: {:#?}", preds.predicates)
-     };
-     return format!("\nParams: {:#?}{preds_string}{parent_chain}", params);
+// TODO: consider using underlying structs struct GenericData<'a>(Vec<(&'a Generics,GenericPredicates<'a>)>);
+#[derive(Serialize)]
+struct GenericData(Vec<(String,String)>);
+#[derive(Serialize)]
+struct ItemDetails {
+    internal_kind: String,
+    path: String,
+    internal_ty: String,
+    generic_data: GenericData,
+}
+#[derive(Serialize)]
+struct BodyDetails {
+    pp: String,
+}
+
+#[derive(Serialize)]
+struct MirBody(Body, Option<BodyDetails>);
+#[derive(Serialize)]
+struct Item {
+    name: String,
+    kind: ItemKind,
+    body: MirBody,
+    promoted: Vec<MirBody>,
+    details: Option<ItemDetails>
+}
+
+pub fn generic_data(tcx: TyCtxt<'_>, id: DefId) -> GenericData {
+     let mut v = Vec::new();
+     let mut next_id = Some(id);
+     while let Some(curr_id) = next_id {
+        let params = tcx.generics_of(id);
+        let preds  = tcx.predicates_of(id);
+        if params.parent != preds.parent { panic!("Generics and predicates parent ids are distinct"); }
+        v.push((format!("{:#?}", params), format!("{:#?}", preds)));
+        next_id = params.parent;
+     }
+     v.reverse();
+     return GenericData(v);
+}
+
+pub fn get_body_details(body: &Body, name: Option<&String>) -> Option<BodyDetails> {
+  if enabled!(tracing::Level::DEBUG) {
+    let mut v = Vec::new();
+    let name = if let Some(name) = name { name } else { "<promoted>" };
+    body.dump(&mut v, name);
+    Some(BodyDetails {
+      pp: str::from_utf8(&v).unwrap().into(),
+    })
   } else {
-    return "".into()
-  }
-}
-
-pub fn print_body(body: &stable_mir::mir::Body, name: &String, out: &mut io::Stdout) {
-  // Default SMIR Printer
-  body.dump(out, name);
-  // Custom Local Printer
-  // function_body(out, &item.body(), &item.name());
-  // Debug Printer
-  println!("{:#?}", body);
-  // JSON Printer
-  println!("{}",to_json(body).unwrap());
-}
-
-pub fn print_item(tcx: TyCtxt<'_>, item: &stable_mir::CrateItem, out: &mut io::Stdout) {
-  print_body(&item.body(), &item.name(), out);
-  for (idx, promoted) in tcx.promoted_mir(rustc_internal::internal(tcx,item.def_id())).into_iter().enumerate() {
-    let promoted_body = rustc_internal::stable(promoted);
-    print_body(&promoted_body, (&format!("promoted[{}:{}]", &item.name(), idx)).into(), out);
+    None
   }
 }
 
@@ -79,36 +101,17 @@ pub fn print_type<'tcx>(tcx: TyCtxt<'tcx>, id: DefId, ty: EarlyBinder<Ty<'tcx>>)
   }
 }
 
-pub fn print_item_details(tcx: TyCtxt<'_>, id: DefId, item: &stable_mir::CrateItem) {
-  // Internal Details
-  //
-  // get DefKind for item
-  let internal_kind: DefKind = tcx.def_kind(id);
-  // get DefPath for item
-  let path: DefPath = tcx.def_path(id);
-  // get string version of DefPath
-  let path_str: String = tcx.def_path_str(id);
-  // get type, generic parameters, required predicates, layout
-  let ty_str = print_type(tcx, id, tcx.type_of(id));
-  let generics_chain = print_generics_chain(tcx, Some(id));
-  // let layout = tcx.layout_of(id);
-  println!("===Internal===\nDefId: {:#?}\nDefKind: {:#?}\nDefPath: {:#?}\nDefPathStr: {}{ty_str}{generics_chain}",
-           id, internal_kind, path, path_str);
-
-  // Stable Details
-  //
-  // get stable MIR kind
-  let kind = item.kind();
-  // get MIR Symbol
-  let name: Symbol = item.name();
-  println!("===SMIR===\nkind: {:#?}\nname: {:#?}",
-           kind, name);
-}
-
-pub fn print_all_items(tcx: TyCtxt<'_>) {
-  let mut out = io::stdout();
-  for item in stable_mir::all_local_items().iter() {
-    print_item(tcx, item, &mut out);
+pub fn get_item_details(tcx: TyCtxt<'_>, id: DefId) -> Option<ItemDetails> {
+  if enabled!(tracing::Level::DEBUG) {
+    Some(ItemDetails {
+      internal_kind: format!("{:#?}", tcx.def_kind(id)),
+      path: tcx.def_path_str(id),  // NOTE: underlying data from tcx.def_path(id);
+      internal_ty: print_type(tcx, id, tcx.type_of(id)),
+      generic_data: generic_data(tcx, id),
+      // TODO: let layout = tcx.layout_of(id);
+    })
+  } else {
+    None
   }
 }
 
@@ -116,12 +119,26 @@ pub fn has_attr(tcx: TyCtxt<'_>, item: &stable_mir::CrateItem, attr: Symbol) -> 
    tcx.has_attr(rustc_internal::internal(tcx,item), sym::test)
 }
 
-pub fn print_all_items_verbose(tcx: TyCtxt<'_>) {
+fn mkMirBody(body: Body, name: Option<&String>) -> MirBody {
+  let details = get_body_details(&body, name);
+  MirBody(body, details)
+}
+
+// TODO: Should we filter any incoming items?
+//       Example: .filter(|item| has_attr(item, sym::test) or matches!(item.kind, ItemKind::Const | ItemKind::Static | ItemKind::Fn))
+pub fn print_all_items(tcx: TyCtxt<'_>) {
   let mut out = io::stdout();
-  // find entrypoints and constants
-  for item in stable_mir::all_local_items().iter() { // .filter(|item| has_attr(item, sym::test) or matches!(item.kind, ItemKind::Const | ItemKind::Static | ItemKind::Fn))  {
-    let id = rustc_internal::internal(tcx, item);
-    print_item_details(tcx, id, item);
-    print_item(tcx, item, &mut out);
-  }
+  let items: Vec<Item> = stable_mir::all_local_items().iter().map(|item| {
+    let name = format!("{:?}", item.name());
+    let body = item.body();
+    let id = rustc_internal::internal(tcx,item.def_id());
+    Item {
+      name: name,
+      kind: item.kind(),
+      body: mkMirBody(body, Some(&name)),
+      promoted: tcx.promoted_mir(id).into_iter().map(|body| mkMirBody(rustc_internal::stable(body), None)).collect(),
+      details: get_item_details(tcx, id),
+    }
+  }).collect();
+  println!("{}", to_json(items).expect("serde_json failed"));
 }
