1fc11eeecd8927e407311c6f9e035cf3e5f8f9df
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2024-11-01 12:45:02 -0400

    remove imported kani code

--- src/kani_lib/mod.rs
+++
@@ -1,7 +0,0 @@
-// This module wraps code derived from the kani project
-//
-// https://github.com/model-checking/kani
-//
-// Code used under license; see license file in the module directory.
-
-pub mod kani_collector;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![feature(rustc_private)]
 pub mod driver;
 pub mod printer;
-pub mod kani_lib;
 pub use driver::stable_mir_driver;
 pub use printer::*;
