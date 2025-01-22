07282312a5bce4c0945538f8902a729754d541ef
Jost Berthold <jost.berthold@gmail.com>
2025-01-22 16:04:58 +1100

    Main program to convert smir json to a dot file

--- scratch/smir-cfg/app/Main.hs
+++ scratch/smir-cfg/app/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
+import Data.Aeson qualified as Json
+import Data.ByteString.Lazy qualified as BS
+import Data.Text.IO qualified as Text
+import System.Environment
+
 import Data.Smir.Json ()
+import Data.Smir.Cfg
+import Data.Smir.Construct
 
 main :: IO ()
-main = undefined
+main = getArgs >>= mapM_ process
+
+process :: FilePath -> IO ()
+process file = do
+    BS.readFile file >>=
+        either error (pure . render . construct) . Json.eitherDecode >>=
+        Text.writeFile (file <> ".dot")
