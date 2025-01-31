cf5d589e3ce3a0c4cecb085b549a864b46344fbe
dkcumming <daniel.cumming@runtimeverification.com>
2025-01-31 16:30:06 +1000

    Editted files to conform to `cargo fmt`

--- src/driver.rs
+++ src/driver.rs
@@ -17,36 +17,32 @@
 //!
 //! However, we prefer a non-macro version for clarity and build simplicity.
 
-extern crate rustc_middle;
 extern crate rustc_driver;
 extern crate rustc_interface;
-extern crate rustc_smir;
+extern crate rustc_middle;
 extern crate rustc_session;
-use rustc_middle::ty::TyCtxt;
+extern crate rustc_smir;
 use rustc_driver::Compilation;
 use rustc_interface::interface::Compiler;
+use rustc_middle::ty::TyCtxt;
 use rustc_smir::rustc_internal;
 
 struct StableMirCallbacks {
-  callback_fn: fn (TyCtxt) -> (),
+    callback_fn: fn(TyCtxt) -> (),
 }
 
 impl rustc_driver::Callbacks for StableMirCallbacks {
-    fn after_analysis(
-        &mut self,
-        _compiler: &Compiler,
-        tcx: TyCtxt,
-    ) -> Compilation {
-
+    fn after_analysis(&mut self, _compiler: &Compiler, tcx: TyCtxt) -> Compilation {
         let _ = rustc_internal::run(tcx, || (self.callback_fn)(tcx));
 
         Compilation::Continue
     }
 }
 
-pub fn stable_mir_driver(args_outer: &[String], callback_fn: fn (TyCtxt) -> ()) {
+pub fn stable_mir_driver(args_outer: &[String], callback_fn: fn(TyCtxt) -> ()) {
     let mut callbacks = StableMirCallbacks { callback_fn };
-    let early_dcx = rustc_session::EarlyDiagCtxt::new(rustc_session::config::ErrorOutputType::default());
+    let early_dcx =
+        rustc_session::EarlyDiagCtxt::new(rustc_session::config::ErrorOutputType::default());
     rustc_driver::init_rustc_env_logger(&early_dcx);
     let _ = rustc_driver::RunCompiler::new(args_outer, &mut callbacks).run();
 }
