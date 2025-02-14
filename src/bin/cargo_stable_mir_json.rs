d887f9c2e990d2bb37f2d76033cec7f08170b1a7
dkcumming <daniel.cumming@runtimeverification.com>
2025-02-14 16:57:04 +1000

    User can provide location for`.stable_mir_json`

--- src/bin/cargo_stable_mir_json.rs
+++ src/bin/cargo_stable_mir_json.rs
@@ -8,74 +8,56 @@ use anyhow::{bail, Result};
 
 fn main() -> Result<()> {
     let args: Vec<_> = env::args().collect();
-    if args.len() != 2 {
-        bail!("Usage: cargo run --bin cargo_stable_mir_json -- <PATH-TO-STABLE-MIR-JSON-REPO>")
-    }
-    let repo_path: PathBuf = args[1].clone().into();
-    if !repo_path.is_dir() {
+
+    let (repo_dir, maybe_user_provided_dir): (PathBuf, Option<PathBuf>) = match &args[..] {
+        [_, repo_dir] => (repo_dir.into(), None),
+        [_, repo_dir, user_provided_dir] => (repo_dir.into(), Some(user_provided_dir.into())),
+        _ => bail!("Usage: cargo run --bin cargo_stable_mir_json -- <PATH-TO-STABLE-MIR-JSON-REPO> [OPTIONAL-PATH-TO-CREATE-BUILD-DIR]"),
+    };
+
+    if !repo_dir.is_dir() {
         bail!(
             "Provided should be path to stable_mir_json repo, but {} is not a dir",
-            repo_path.display()
+            repo_dir.display()
         );
     }
 
-    let hidden_dir = setup(repo_path)?;
-    record_ld_library_path(&hidden_dir)?;
-    add_run_script(&hidden_dir)
-}
-
-fn add_run_script(hidden_dir: &Path) -> Result<()> {
-    let run_script_path = hidden_dir.join("run.sh");
-    let mut run_script = std::fs::File::create(&run_script_path)?;
-    writeln!(run_script, "#!/bin/bash")?;
-    writeln!(run_script, "set -eu")?;
-    writeln!(run_script)?;
-    writeln!(
-        run_script,
-        "export LD_LIBRARY_PATH=$(cat ~/.stable_mir_json/ld_library_path)"
-    )?;
-    writeln!(
-        run_script,
-        "exec \"$HOME/.stable_mir_json/debug/stable_mir_json\" \"$@\""
-    )?;
-
-    // Set the script permissions to -rwxr-xr-x
-    std::fs::set_permissions(run_script_path, std::fs::Permissions::from_mode(0o755))?;
-    Ok(())
-}
-
-fn record_ld_library_path(hidden_dir: &Path) -> Result<()> {
-    const LOADER_PATH: &str = "LD_LIBRARY_PATH";
-    if let Some(paths) = env::var_os(LOADER_PATH) {
-        // Note: kani filters the LD_LIBRARY_PATH, not sure why as it is working locally as is
-        let mut ld_library_file = std::fs::File::create(hidden_dir.join("ld_library_path"))?;
-        writeln!(ld_library_file, "{}", paths.to_str().unwrap())?;
-    } else {
-        bail!("Couldn't read LD_LIBRARY_PATH from env");
+    if let Some(ref user_provided_dir) = maybe_user_provided_dir {
+        if !user_provided_dir.is_dir() {
+            bail!(
+                "Provided should be path to create the .stable_mir_json dir, but {} is not a dir",
+                user_provided_dir.display()
+            );
+        }
     }
 
-    Ok(())
+    setup(repo_dir, maybe_user_provided_dir)
 }
 
-fn smir_json_dir() -> Result<PathBuf> {
-    let home_dir = home::home_dir().expect("couldn't find home directory");
-    if !home_dir.is_dir() {
-        bail!(
-            "got home directory `{}` which isn't a directory",
-            home_dir.display()
-        );
-    }
-    let smir_json_dir = home_dir.join(".stable_mir_json");
-    Ok(smir_json_dir)
-}
-
-fn setup(repo_dir: PathBuf) -> Result<PathBuf> {
-    let smir_json_dir = smir_json_dir()?;
+fn setup(repo_dir: PathBuf, maybe_user_provided_dir: Option<PathBuf>) -> Result<()> {
+    let smir_json_dir = smir_json_dir(maybe_user_provided_dir)?;
     println!("Creating {} directory", smir_json_dir.display());
-    std::fs::create_dir(&smir_json_dir)?;
+    std::fs::create_dir(&smir_json_dir)?; // This errors is the directory already exists
 
     copy_artefacts(&repo_dir, &smir_json_dir)?;
 
+    record_ld_library_path(&smir_json_dir)?;
+    add_run_script(&smir_json_dir)
+}
+
+fn smir_json_dir(maybe_user_provided_dir: Option<PathBuf>) -> Result<PathBuf> {
+    let user_provided_dir = match maybe_user_provided_dir {
+        Some(user_provided_dir) => user_provided_dir,
+        None => home::home_dir().expect("couldn't find home directory"),
+    };
+    if !user_provided_dir.is_dir() {
+        bail!(
+            // We know this is home because main checked user_provided_dir already
+            "got home directory `{}` which isn't a directory",
+            user_provided_dir.display()
+        );
+    }
+    let smir_json_dir = user_provided_dir.join(".stable_mir_json");
     Ok(smir_json_dir)
 }
 
@@ -175,6 +157,39 @@ fn cp_artefacts_from_profile(
                 }
             }
         }
+    }
+
+    Ok(())
+}
+
+fn add_run_script(smir_json_dir: &Path) -> Result<()> {
+    let run_script_path = smir_json_dir.join("run.sh");
+    let mut run_script = std::fs::File::create(&run_script_path)?;
+    writeln!(run_script, "#!/bin/bash")?;
+    writeln!(run_script, "set -eu")?;
+    writeln!(run_script)?;
+    writeln!(
+        run_script,
+        "export LD_LIBRARY_PATH=$(cat ~/.stable_mir_json/ld_library_path)"
+    )?;
+    writeln!(
+        run_script,
+        "exec \"/home/daniel/.stable_mir_json/debug/stable_mir_json\" \"$@\""
+    )?;
+
+    // Set the script permissions to -rwxr-xr-x
+    std::fs::set_permissions(run_script_path, std::fs::Permissions::from_mode(0o755))?;
+    Ok(())
+}
+
+fn record_ld_library_path(smir_json_dir: &Path) -> Result<()> {
+    const LOADER_PATH: &str = "LD_LIBRARY_PATH";
+    if let Some(paths) = env::var_os(LOADER_PATH) {
+        // Note: kani filters the LD_LIBRARY_PATH, not sure why as it is working locally as is
+        let mut ld_library_file = std::fs::File::create(smir_json_dir.join("ld_library_path"))?;
+        writeln!(ld_library_file, "{}", paths.to_str().unwrap())?;
+    } else {
+        bail!("Couldn't read LD_LIBRARY_PATH from env");
     }
 
     Ok(())
